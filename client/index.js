bc621db3a3a033f888e0b95dd4fce5291a1c3978
Manuj Haritwal <manuj.haritwal01@gmail.com>
2024-07-04 20:25:53 +0530

    remove lazyloading for vercel deployment

--- client/index.js
+++ client/index.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client'
 import App from "./src/app"
-import { lazy, Suspense } from "react"
+// import { lazy, Suspense } from "react"
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import RestaurantMenu from "./src/components/RestaurantMenu"
 import Body from './src/components/Body';
-import Loading from './src/components/Loading';
+// import Loading from './src/components/Loading';
 import Error from './src/components/Error';
-const About = lazy(() => import('./src/components/About'))
-const Cart = lazy(() => import('./src/components/Cart'))
-const RestaurantMenu = lazy(() => import('./src/components/RestaurantMenu'))
+import About from './src/components/About';
+import Cart from './src/components/Cart';
+import RestaurantMenu from './src/components/RestaurantMenu';
+// const About = lazy(() => import('./src/components/About'))
+// const Cart = lazy(() => import('./src/components/Cart'))
+// const RestaurantMenu = lazy(() => import('./src/components/RestaurantMenu'))
 
 const root = ReactDOM.createRoot(document.getElementById("root"))
 
@@ -24,13 +27,13 @@ const appRouter = createBrowserRouter([
                 element: <Body />
             }, {
                 path: '/about',
-                element: <Suspense fallback={<Loading />}><About /></Suspense>
+                element: <About /> //<Suspense fallback={<Loading />}><About /></Suspense>
             }, {
                 path: '/cart',
-                element: <Suspense fallback={<Loading />}><Cart /></Suspense>
+                element: <Cart />//<Suspense fallback={<Loading />}><Cart /></Suspense>
             }, {
                 path: '/restaurant/:id',
-                element: <Suspense fallback={<Loading />}><RestaurantMenu /></Suspense>
+                element: <RestaurantMenu /> //<Suspense fallback={<Loading />}><RestaurantMenu /></Suspense>
             }
         ]
     }
