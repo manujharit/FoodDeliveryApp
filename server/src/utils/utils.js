4ff58c2655ba431b7d1b0809f936824978e472b9
Manuj Haritwal <manuj.haritwal01@gmail.com>
2024-08-01 19:54:53 +0530

    chnage minor logic

--- server/src/utils/utils.js
+++ server/src/utils/utils.js
@@ -3,10 +3,10 @@
 const filterData = (data) => {
     const restaurantsData = {}
 
-    restaurantsData['whats_on_your_mind'] = data.filter((card) => card.card.card.id === "whats_on_your_mind")[0].card.card.gridElements.infoWithStyle.info
-    restaurantsData['top_brands_for_you'] = data.filter((card) => card.card.card.id === "top_brands_for_you")[0].card.card.gridElements.infoWithStyle.restaurants.map((card) => card.info)
-    restaurantsData['restaurant_grid_listing'] = data.filter((card) => card.card.card.id === 'restaurant_grid_listing')[0].card.card.gridElements.infoWithStyle.restaurants.map((card) => card.info)
-    restaurantsData['restaurant_near_me_links'] = data.filter(card => card.card.card.id === 'restaurant_near_me_links').map(res => { return { title: res.card.card.title, brands: res.card.card.brands } })
+    restaurantsData['whats_on_your_mind'] = data?.filter((card) => card.card.card.id === "whats_on_your_mind")[0]?.card?.card?.gridElements?.infoWithStyle?.info
+    restaurantsData['top_brands_for_you'] = data?.filter((card) => card.card.card.id === "top_brands_for_you")[0]?.card?.card?.gridElements?.infoWithStyle?.restaurants?.map((card) => card.info)
+    restaurantsData['restaurant_grid_listing'] = data?.filter((card) => card.card.card.id === 'restaurant_grid_listing')[0]?.card?.card?.gridElements?.infoWithStyle?.restaurants?.map((card) => card.info)
+    restaurantsData['restaurant_near_me_links'] = data?.filter(card => card.card.card.id === 'restaurant_near_me_links')?.map(res => { return { title: res.card.card.title, brands: res.card.card.brands } })
 
     return restaurantsData
 }
@@ -14,9 +14,9 @@ const filterData = (data) => {
 const filterDataByTags = (data) => {
 
     let restaurantData = {}
-    restaurantData['count'] = data.filter(card => card?.card?.card['@type'] === "type.googleapis.com/swiggy.gandalf.widgets.v2.InlineViewFilterSortWidget")[0]?.card?.card?.restaurantCount
-    restaurantData['title'] = data.filter(card => card?.card?.card?.type === "COLLECTION_MASTHEAD_TYPE_IMAGE_WITH_TEXT")[0]?.card?.card
-    restaurantData['restaurants'] = data.filter(card => card?.card?.card['@type'] === "type.googleapis.com/swiggy.presentation.food.v2.Restaurant").map(card=>card?.card?.card?.info)
+    restaurantData['count'] = data?.filter(card => card?.card?.card['@type'] === "type.googleapis.com/swiggy.gandalf.widgets.v2.InlineViewFilterSortWidget")[0]?.card?.card?.restaurantCount
+    restaurantData['title'] = data?.filter(card => card?.card?.card?.type === "COLLECTION_MASTHEAD_TYPE_IMAGE_WITH_TEXT")[0]?.card?.card
+    restaurantData['restaurants'] = data?.filter(card => card?.card?.card['@type'] === "type.googleapis.com/swiggy.presentation.food.v2.Restaurant")?.map(card=>card?.card?.card?.info)
     return restaurantData
 }
 
@@ -29,7 +29,7 @@ const filterUpdateData = (data) => {
     return resData
 }
 const filterMenuData = (data) => {
-    const menuData = data?.filter(card => card.groupedCard)[0].groupedCard?.cardGroupMap?.REGULAR?.cards.filter(card => card?.card?.card?.['@type'] === 'type.googleapis.com/swiggy.presentation.food.v2.ItemCategory').map(data => ({ title: data.card.card.title, itemCards: data.card.card.itemCards }))
+    const menuData = data?.filter(card => card.groupedCard)[0].groupedCard?.cardGroupMap?.REGULAR?.cards?.filter(card => card?.card?.card?.['@type'] === 'type.googleapis.com/swiggy.presentation.food.v2.ItemCategory')?.map(data => ({ title: data.card.card.title, itemCards: data.card.card.itemCards }))
     return menuData
 }
 
